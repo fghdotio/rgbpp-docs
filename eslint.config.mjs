d7caf7c802e9939dfcb337c70c950fcb86b66d26
fgh <fghdotio@gmail.com>
2025-06-24 10:07:45 -0500

    chore: relax eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "ignoreRestSiblings": true
+      }],
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+      "prefer-const": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
